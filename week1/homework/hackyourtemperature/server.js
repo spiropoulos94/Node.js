acaacab3ec9b21b1ee29e7c3ff6ae7ac6f9f8782
Nikos Spiropoulos <spiropoulos94@gmail.com>
2020-03-19 18:11:14 +0200

    hack your temperature week 1 & 2 update

--- week1/homework/hackyourtemperature/server.js
+++ week1/homework/hackyourtemperature/server.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const app = express()
+var exphbs = require('express-handlebars');
 
-app.get('/', function (req, res) {
-    res.send('hello from backend to frontend!')
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+app.set('view engine', 'handlebars');
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+
+
+
+
+
+app.get("/", (req, res) => {
+    res.render('index')
 })
 
-app.listen(3000)
+app.post("/weather", (req, res) => {
+    const cityName = req.body.cityName
+    res.send(cityName)
+})
+
+app.listen(3000, () => { console.log("It's Alive!!!!!") })
+
